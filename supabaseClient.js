e2e3c192cd269431c499d4dbbce49c7ee7dc98d2
biznara <31770170+biznara@users.noreply.github.com>
2025-09-04 12:05:06 +0900

    Auto update on 2025-09-04 12:05:03.56

---
+++ supabaseClient.js
@@ -0,0 +1,9 @@
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
+
+// 👉 Supabase 대시보드 Project Settings → API 에서 복사
+const SUPABASE_URL = "https://snxjcbaaysgfunpsohzg.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNueGpjYmFheXNnZnVucHNvaHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1OTk3MzQsImV4cCI6MjA3MjE3NTczNH0.T8b9PpabXkCvwW2W57Qbr-h--JLZB6errlyP5IwsYyk"
+
+// 클라이언트 생성
+export const supabase = createClient(https://snxjcbaaysgfunpsohzg.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNueGpjYmFheXNnZnVucHNvaHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1OTk3MzQsImV4cCI6MjA3MjE3NTczNH0.T8b9PpabXkCvwW2W57Qbr-h--JLZB6errlyP5IwsYyk)
