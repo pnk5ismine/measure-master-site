8dfe6ba5b8978407855b303d24b82c104a95c69c
biznara <31770170+biznara@users.noreply.github.com>
2025-09-19 22:03:00 +0900

    Auto update on 2025-09-19 22:03:00.00

---
+++ build-app.bat
@@ -0,0 +1,8 @@
+REM [1] Project root where gradlew.bat lives.
+set "PROJECT_ROOT=D:\Android_Home\AndroidStudioProjects\Compose_Study1\MMProject"  rem ← 여기만 바꿔도 보통 끝
+
+REM [2] Wrapper가 부모 폴더에 있으면 1
+set "GRADLE_AT_PARENT=0"
+
+REM [3] 모듈명(멀티 프로젝트에서만 사용)
+set "MODULE_NAME=MMProject"
